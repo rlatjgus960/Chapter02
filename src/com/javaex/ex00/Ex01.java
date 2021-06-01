0bdf2f2000f9a60061c09ac49a1a52cf65676c5a
rlatjgus960 <rlatjgus960@naver.com>
2021-06-01 18:01:26 +0900

    6/1 자바 프로그래밍

---
+++ src/com/javaex/ex00/Ex01.java
@@ -0,0 +1,22 @@
+package com.javaex.ex00;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+
+		/* 적은 수는 괜찮지만 많은 데이터를 관리하기에는 일일히 입력하기 불편함
+		String name = "니콘";
+		int price = 400000;
+		
+		System.out.println(name+","+price);
+		
+		String name2="그램";
+		int price2 = 1000000;
+		
+		System.out.println(name2+","+price2);
+		*/
+		
+
+	}
+
+}
