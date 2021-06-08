61967b64d7fb30d701046cca53403bbfafd69b07
rlatjgus960 <rlatjgus960@naver.com>
2021-06-08 17:32:59 +0900

    6/8

--- src/com/javaex/ex17/ShapeApp.java
+++ src/com/javaex/ex17/ShapeApp.java
@@ -28,6 +28,7 @@ public class ShapeApp {
 		
 		Rectangle[] rArray = new Rectangle[3];
 		
+		/*
 		Rectangle r01 = new Rectangle("빨강", "검정",3,3);
 		Rectangle r02 = new Rectangle("주황", "검정",4,4);
 		Rectangle r03 = new Rectangle("초록", "검정",3,3);
@@ -35,6 +36,12 @@ public class ShapeApp {
 		rArray[0] = r01;
 		rArray[1] = r02;
 		rArray[2] = r03; //넣는거는 노가다 해야됨
+		*/
+		
+		rArray[0] = new Rectangle("빨강", "검정",3,3);
+		rArray[1] = new Rectangle("주황", "검정",4,4);
+		rArray[2] = new Rectangle("초록", "검정",3,3);
+		
 		
 		for(int i=0; i<rArray.length; i++) {
 			rArray[i].draw();
@@ -50,6 +57,7 @@ public class ShapeApp {
 		
 		Circle[] cArray = new Circle[3];
 		
+		/*
 		Circle c01 = new Circle("빨강", "검정",3);
 		Circle c02 = new Circle("노랑", "검정",4);
 		Circle c03 = new Circle("파랑", "검정",5);
@@ -57,6 +65,12 @@ public class ShapeApp {
 		cArray[0] = c01;
 		cArray[1] = c02;
 		cArray[2] = c03;
+		*/
+		
+		cArray[0] = new Circle("빨강", "검정",3);
+		cArray[1] = new Circle("노랑", "검정",4);
+		cArray[2] = new Circle("파랑", "검정",5);
+	
 		
 		
 		for (int i=0; i<cArray.length; i++) {
@@ -69,6 +83,7 @@ public class ShapeApp {
 		
 		Triangle[] tArray = new Triangle[3];
 		
+		/*
 		Triangle t01 = new Triangle("초록", "검정",3,3);
 		Triangle t02 = new Triangle("보라", "검정",4,4);
 		Triangle t03 = new Triangle("빨강", "검정",5,5);
@@ -76,6 +91,12 @@ public class ShapeApp {
 		tArray[0] = t01;
 		tArray[1] = t02;
 		tArray[2] = t03;
+		*/
+		
+		tArray[0] = new Triangle("초록", "검정",3,3);
+		tArray[1] = new Triangle("보라", "검정",4,4);
+		tArray[2] = new Triangle("빨강", "검정",5,5);
+		
 		
 		
 		for (int i=0; i<tArray.length; i++) {
